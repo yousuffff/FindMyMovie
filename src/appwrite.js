9d228c81c88e977699c83a7c3fbe3e422c4f7871
Mohd Yousuf <mdyousuf43201@gmail.com>
2026-02-17 23:21:06 +0530

    add appwrite

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,6 +1,38 @@
+import { Client, Databases, Query ,ID} from "appwrite";
+import { data } from "react-router-dom";
+
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
-export const updateSearchCount = async() =>{
-  console.log(PROJECT_ID,DATABASE_ID,COLLECTION_ID)
-}
+const ENDPOINT = import.meta.env.VITE_APPWRITE_ENDPOINT;
+const client = new Client().setEndpoint(ENDPOINT).setProject(PROJECT_ID);
+
+const database = new Databases(client);
+
+export const updateSearchCount = async (searchText , movie) => {
+  // console.log(PROJECT_ID, DATABASE_ID, COLLECTION_ID);
+  try {
+
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID ,q[Query.equal(attribute: 'searchTerm', searchText)])
+
+    if(result.documents.length > 0){
+      const doc = result.documents[0]
+
+      await database.updateTransaction(DATABASE_ID , COLLECTION_ID ,  doc.$id, {
+        count = doc.count + 1
+      })
+
+    }else{
+await database.createTransaction(DATABASE_ID, COLLECTION_ID, ID.unique(),{
+  count: 1,
+  movie_id: movie.id,
+  poster_url: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+})
+    }
+    
+  } catch (error) {
+    console.log(error);
+    
+  }
+};
+
